4d3b0483f8c88e60f38d62e09722a2881434d7e5
suryasai <suryasaigopalam@gmail.com>
2024-03-10 19:27:42 +0530

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by surya sai on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
